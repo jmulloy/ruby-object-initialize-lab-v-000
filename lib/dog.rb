2692fbb2fb4f8717e3fdf3625df2708207728a58
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-07 20:41:27 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+def initialize(name)
+  @name = name
+end
+def name=(name)
+  @name = name
+end
+fido = Dog.new
+fido.name = "Fido"
+puts fido.name
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
